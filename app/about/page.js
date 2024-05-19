e5324909cafb236af0a9e23192d8c4f8f8f9d4f4
Irina Saveleva <goyalion17@gmail.com>
2024-05-19 16:45:53 +0200

    creating a layout

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,4 @@
-import Navigation from "../../component/Navigation";
+import Navigation from "../../components/Navigation";
 
 function AboutPage() {
   return (
--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
-import Navigation from "../component/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The Wold Oasis. Welcome to paradise</h1>
     </div>
   );
