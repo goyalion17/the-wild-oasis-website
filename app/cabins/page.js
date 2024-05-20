84497648da4d06afd24cc0c8a6d8ef29bcebff52
Irina Saveleva <goyalion17@gmail.com>
2024-05-20 17:18:46 +0200

    project organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,20 +1,7 @@
-import Counter from "../../components/Counter";
-
-export default async function CabinsPage() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
+export default function CabinsPage() {
   return (
     <div>
       <h1>CabinsPage</h1>
-
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      <Counter users={data} />
     </div>
   );
 }
